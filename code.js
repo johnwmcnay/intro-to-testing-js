26e60d402c6e969c9a0d701d1624abb432369451
John McNay <john.w.mcnay@gmail.com>
2020-07-28 15:20:15 -0500

    through exercise 5

--- code.js
+++ code.js
@@ -6,5 +6,7 @@ const helloWorld = () => {
 const sayHello = name => {
     if (name === "Alex")
         return "Hello, Alex!";
+    if (name === "Pat")
+        return "Hello, Pat!";
     return "Hello, Jane!";
 }
